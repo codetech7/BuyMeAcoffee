c536773b30069a876781f6c88d69a856a17332fe
codetech7 <codetechseven@gmail.com>
2022-07-26 00:57:37 +0100

    Included goerli configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli:{
+      URL:  GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+
+  }
 };
